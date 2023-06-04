824e85d933efb47a9beaf3168183781691ee83fe
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-06-04 21:27:41 +0200

    Header Component added

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,44 @@
+import styles from './Header.module.scss';
+
+function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <img src="/src/assets/images/logo.png" alt="Sufle" />
+        
+        <nav className={styles.nav}>
+          <ul>
+            <li className={styles.active}>
+              <a href="#">Ana Səhifə</a>
+            </li>
+            <li>
+              <a href="#">Məhsullar</a>
+            </li>
+            <li>
+              <a href="#">Qalereya</a>
+            </li>
+            <li>
+              <a href="#">Mağazalarımız</a>
+            </li>
+            <li>
+              <a href="#">Haqqımızda</a>
+            </li>
+            <li>
+              <a href="#">Əlaqə</a>
+            </li>
+            <li>
+              <a href="#">Vakansiya</a>
+            </li>
+          </ul>
+        </nav>
+        
+        <div className={styles.search}>
+          <img src="/src/assets/images/icons/search.svg" alt="Search" />
+          <input type="text" placeholder="Axtarış..." />
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
