23ada9224cfe9844660d8a05b4e43ce8ecc60dcd
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-06-05 17:12:32 +0200

    Title Component added

---
+++ src/components/Title/Title.tsx
@@ -0,0 +1,18 @@
+import styles from './Title.module.scss';
+
+interface Props {
+  title: string;
+  subtitle: string;
+}
+
+function Title({ title, subtitle }: Props) {
+  return (
+    <div className={styles.container}>
+      <hr />
+      <p>{title}</p>
+      <h1>{subtitle}</h1>
+    </div>
+  );
+}
+
+export default Title;
