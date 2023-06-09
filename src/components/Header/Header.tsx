5eee9e0e2102d120ee91397ff55c6b220a50ae5f
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-06-09 14:30:17 +0200

    Mobile Navbar added

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,37 +1,83 @@
 import styles from './Header.module.scss';
+import { useState } from 'react';
+
+import cs from 'classnames';
 
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <header className={styles.header}>
       <div className={styles.container}>
-        <button className={styles.menuBtn}>
+        <button className={styles.openBtn} onClick={() => setMenuOpen(!menuOpen)}>
           <img src="/src/assets/images/icons/menu.svg" alt="Menu" />
         </button>
 
         <img className={styles.logo} src="/src/assets/images/logo.png" alt="Sufle" />
 
-        <nav className={styles.nav}>
+        <nav className={cs(styles.nav, { [styles.navOpen]: menuOpen })}>
+          <div className={styles.navHeader}></div>
           <ul>
+            <li className={styles.navHeader}>
+              <img className={styles.navLogo} src="/src/assets/images/logo.png" alt="Sufle" />
+
+              <button className={styles.closeBtn} onClick={() => setMenuOpen(!menuOpen)}>
+                <img src="/src/assets/images/icons/close.svg" alt="Close" />
+              </button>
+            </li>
             <li className={styles.active}>
-              <a href="#">Ana Səhifə</a>
+              <a href="#">
+                Ana Səhifə
+                <img src="/src/assets/images/icons/arrow.svg" alt="Arrow" />
+              </a>
             </li>
             <li>
-              <a href="#">Məhsullar</a>
+              <a href="#">
+                Məhsullar
+                <img src="/src/assets/images/icons/arrow.svg" alt="Arrow" />
+              </a>
             </li>
             <li>
-              <a href="#">Qalereya</a>
+              <a href="#">
+                Qalereya
+                <img src="/src/assets/images/icons/arrow.svg" alt="Arrow" />
+              </a>
             </li>
             <li>
-              <a href="#">Mağazalarımız</a>
+              <a href="#">
+                Mağazalarımız
+                <img src="/src/assets/images/icons/arrow.svg" alt="Arrow" />
+              </a>
             </li>
             <li>
-              <a href="#">Haqqımızda</a>
+              <a href="#">
+                Haqqımızda
+                <img src="/src/assets/images/icons/arrow.svg" alt="Arrow" />
+              </a>
             </li>
             <li>
-              <a href="#">Əlaqə</a>
+              <a href="#">
+                Əlaqə
+                <img src="/src/assets/images/icons/arrow.svg" alt="Arrow" />
+              </a>
             </li>
             <li>
-              <a href="#">Vakansiya</a>
+              <a href="#">
+                Vakansiya
+                <img src="/src/assets/images/icons/arrow.svg" alt="Arrow" />
+              </a>
+            </li>
+            <li></li>
+            <li className={styles.navFooter}>
+              <a href="#">
+                <img src="/src/assets/images/icons/facebook.svg" alt="Facebook" />
+              </a>
+              <a href="#">
+                <img src="/src/assets/images/icons/twitter.svg" alt="Twitter" />
+              </a>
+              <a href="#">
+                <img src="/src/assets/images/icons/instagram.svg" alt="Instagram" />
+              </a>
             </li>
           </ul>
         </nav>
