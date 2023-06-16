23e34ff64b79007e16c502262a140bd238602324
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-06-16 15:37:12 +0200

    Vacancy Page added with Responsive

--- src/views/index.tsx
+++ src/views/index.tsx
@@ -1,2 +1,3 @@
 export { default as Contact } from './contact';
 export { default as About } from './about';
+export { default as Vacancy } from './vacancy';
---
+++ src/views/vacancy/index.tsx
@@ -0,0 +1,132 @@
+import Container from '../../components/Container/Container';
+
+import styles from './Vacancy.module.scss';
+
+function Vacancy() {
+  return (
+    <main className={styles.main}>
+      <Container>
+        <div className={styles.wrapper}>
+          <div className={styles.box}>
+            <div className={styles.title}>
+              <hr />
+              <h1>Tort ustası axtarılır !</h1>
+            </div>
+
+            <div className={styles.content}>
+              <p>
+                <b>Tələblər:</b>
+              </p>
+              <ul>
+                <li>
+                  <p>- Qanajlı və marçipanlı tortların bəzədilməsi, rənglənməsi</p>
+                </li>
+                <li>
+                  <p>- Tortların üzrəndə şəkillər çəkmək, yazılar yazmaq, dekor hazırlamaq</p>
+                </li>
+                <li>
+                  <p>- Marçipanlı tortların düzəldilməsi</p>
+                </li>
+                <li>
+                  <p>- Marçipandan fiqurların hazırlanması</p>
+                </li>
+                <li>
+                  <p>- Rəssamlıq, heykəltaraşlıq, şəkər xəmiri İlə işləmək bacarıqları</p>
+                </li>
+              </ul>
+              <p>müraciətlərinizi “Tort ustası” başlığı ilə info@sufle.com mail adresinə göndərməyiniz xahiş olunur</p>
+            </div>
+
+            <div className={styles.media}>
+              <div className={styles.photo}>
+                <div className={styles.overlay}></div>
+                <img src="/src/assets/images/vacancy/vacancy1.png" alt="Tort ustası" />
+                <h2>Tort ustası</h2>
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.box}>
+            <div className={styles.title}>
+              <hr />
+              <h1>Tort ustası axtarılır !</h1>
+            </div>
+
+            <div className={styles.content}>
+              <p>
+                <b>Tələblər:</b>
+              </p>
+              <ul>
+                <li>
+                  <p>- Qanajlı və marçipanlı tortların bəzədilməsi, rənglənməsi</p>
+                </li>
+                <li>
+                  <p>- Tortların üzrəndə şəkillər çəkmək, yazılar yazmaq, dekor hazırlamaq</p>
+                </li>
+                <li>
+                  <p>- Marçipanlı tortların düzəldilməsi</p>
+                </li>
+                <li>
+                  <p>- Marçipandan fiqurların hazırlanması</p>
+                </li>
+                <li>
+                  <p>- Rəssamlıq, heykəltaraşlıq, şəkər xəmiri İlə işləmək bacarıqları</p>
+                </li>
+              </ul>
+              <p>müraciətlərinizi “Tort ustası” başlığı ilə info@sufle.com mail adresinə göndərməyiniz xahiş olunur</p>
+            </div>
+
+            <div className={styles.media}>
+              <div className={styles.photo}>
+                <div className={styles.overlay}></div>
+                <img src="/src/assets/images/vacancy/vacancy1.png" alt="Tort ustası" />
+                <h2>Tort ustası</h2>
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.box}>
+            <div className={styles.title}>
+              <hr />
+              <h1>Tort ustası axtarılır !</h1>
+            </div>
+
+            <div className={styles.content}>
+              <p>
+                <b>Tələblər:</b>
+              </p>
+              <ul>
+                <li>
+                  <p>- Qanajlı və marçipanlı tortların bəzədilməsi, rənglənməsi</p>
+                </li>
+                <li>
+                  <p>- Tortların üzrəndə şəkillər çəkmək, yazılar yazmaq, dekor hazırlamaq</p>
+                </li>
+                <li>
+                  <p>- Marçipanlı tortların düzəldilməsi</p>
+                </li>
+                <li>
+                  <p>- Marçipandan fiqurların hazırlanması</p>
+                </li>
+                <li>
+                  <p>- Rəssamlıq, heykəltaraşlıq, şəkər xəmiri İlə işləmək bacarıqları</p>
+                </li>
+              </ul>
+              <p>müraciətlərinizi “Tort ustası” başlığı ilə info@sufle.com mail adresinə göndərməyiniz xahiş olunur</p>
+            </div>
+
+            <div className={styles.media}>
+              <div className={styles.photo}>
+                <div className={styles.overlay}></div>
+                <img src="/src/assets/images/vacancy/vacancy1.png" alt="Tort ustası" />
+                <h2>Tort ustası</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </main>
+  );
+}
+
+export default Vacancy;
