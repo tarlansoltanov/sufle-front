338f3ef6256fba56eb391809dc33332789afda2c
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-06-18 21:50:52 +0200

    Axios and Axios API instance added

---
+++ src/api/index.tsx
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: 'https://api.sufle.az/api/',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export default instance;
