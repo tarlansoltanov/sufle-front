adbaa6d07a90196e4a41e76f592fef3e5941f26c
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-06-20 14:13:54 +0200

    Header Icon Design fixed

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,103 +10,68 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.container}>
-        <button
-          className={styles.openBtn}
-          onClick={() => setMenuOpen(!menuOpen)}
-        >
+        <button className={styles.openBtn} onClick={() => setMenuOpen(!menuOpen)}>
           <img src="/src/assets/images/icons/menu.svg" alt="Menu" />
         </button>
 
-        <img
-          className={styles.logo}
-          src="/src/assets/images/logo.png"
-          alt="Sufle"
-        />
+        <img className={styles.logo} src="/src/assets/images/logo.png" alt="Sufle" />
 
         <nav className={cs(styles.nav, { [styles.navOpen]: menuOpen })}>
           <div className={styles.navHeader}></div>
 
           <ul>
             <li className={styles.navHeader}>
-              <img
-                className={styles.navLogo}
-                src="/src/assets/images/logo.png"
-                alt="Sufle"
-              />
+              <img className={styles.navLogo} src="/src/assets/images/logo.png" alt="Sufle" />
 
-              <button
-                className={styles.closeBtn}
-                onClick={() => setMenuOpen(!menuOpen)}
-              >
+              <button className={styles.closeBtn} onClick={() => setMenuOpen(!menuOpen)}>
                 <img src="/src/assets/images/icons/close.svg" alt="Close" />
               </button>
             </li>
 
             <li>
-              <NavLink
-                to="/"
-                className={({ isActive }) => (isActive ? styles.active : '')}
-              >
+              <NavLink to="/" className={({ isActive }) => (isActive ? styles.active : '')}>
                 Ana Səhifə
                 <img src="/src/assets/images/icons/arrow.svg" alt="Arrow" />
               </NavLink>
             </li>
 
             <li>
-              <NavLink
-                to="/products"
-                className={({ isActive }) => (isActive ? styles.active : '')}
-              >
+              <NavLink to="/products" className={({ isActive }) => (isActive ? styles.active : '')}>
                 Məhsullar
                 <img src="/src/assets/images/icons/arrow.svg" alt="Arrow" />
               </NavLink>
             </li>
 
             <li>
-              <NavLink
-                to="/gallery"
-                className={({ isActive }) => (isActive ? styles.active : '')}
-              >
+              <NavLink to="/gallery" className={({ isActive }) => (isActive ? styles.active : '')}>
                 Qalereya
                 <img src="/src/assets/images/icons/arrow.svg" alt="Arrow" />
               </NavLink>
             </li>
 
             <li>
-              <NavLink
-                to="/shops"
-                className={({ isActive }) => (isActive ? styles.active : '')}
-              >
+              <NavLink to="/shops" className={({ isActive }) => (isActive ? styles.active : '')}>
                 Mağazalarımız
                 <img src="/src/assets/images/icons/arrow.svg" alt="Arrow" />
               </NavLink>
             </li>
 
             <li>
-              <NavLink
-                to="/about"
-                className={({ isActive }) => (isActive ? styles.active : '')}
-              >
+              <NavLink to="/about" className={({ isActive }) => (isActive ? styles.active : '')}>
                 Haqqımızda
                 <img src="/src/assets/images/icons/arrow.svg" alt="Arrow" />
               </NavLink>
             </li>
 
             <li>
-              <NavLink
-                to="/contact"
-                className={({ isActive }) => (isActive ? styles.active : '')}
-              >
+              <NavLink to="/contact" className={({ isActive }) => (isActive ? styles.active : '')}>
                 Əlaqə
                 <img src="/src/assets/images/icons/arrow.svg" alt="Arrow" />
               </NavLink>
             </li>
 
             <li>
-              <NavLink
-                to="/vacancy"
-                className={({ isActive }) => (isActive ? styles.active : '')}
-              >
+              <NavLink to="/vacancy" className={({ isActive }) => (isActive ? styles.active : '')}>
                 Vakansiya
                 <img src="/src/assets/images/icons/arrow.svg" alt="Arrow" />
               </NavLink>
@@ -116,10 +81,7 @@ const Header = () => {
 
             <li className={styles.navFooter}>
               <a href="#">
-                <img
-                  src="/src/assets/images/icons/facebook.svg"
-                  alt="Facebook"
-                />
+                <img src="/src/assets/images/icons/facebook.svg" alt="Facebook" />
               </a>
 
               <a href="#">
@@ -127,10 +89,7 @@ const Header = () => {
               </a>
 
               <a href="#">
-                <img
-                  src="/src/assets/images/icons/instagram.svg"
-                  alt="Instagram"
-                />
+                <img src="/src/assets/images/icons/instagram.svg" alt="Instagram" />
               </a>
             </li>
           </ul>
