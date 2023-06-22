9abd2209a7b15ab3f19c8bee6f296b7477e7b21f
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-06-22 19:48:37 +0200

    Card Component added with Responsive

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import cs from 'classnames';
+
+import styles from './Card.module.scss';
+
+interface Props {
+  photo: string;
+  name: string;
+  price: number;
+  isNew?: boolean;
+  discount?: number;
+  className?: string;
+  photoClass?: string;
+  style?: React.CSSProperties;
+}
+
+function Card({ photo, name, price, isNew, discount, className, photoClass, style }: Props) {
+  return (
+    <div className={cs(className, styles.card)} style={style}>
+      <div className={cs(photoClass, styles.photo)}>
+        <img src={photo} className={cs(styles.photo)} />
+      </div>
+      <div className={styles.cardInfo}>
+        <div className={styles.title}>{name}</div>
+        <div className={styles.price}>{price} AZN</div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
