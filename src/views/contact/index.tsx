9e8d438eb320e633e92e7088d7dedaead15c2cb0
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-06-22 21:57:53 +0200

    Main Shop API added to Contact page

--- src/views/contact/index.tsx
+++ src/views/contact/index.tsx
@@ -1,4 +1,7 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+import { IShop } from '../../types';
+import { getMainShop } from '../../api';
 
 import Container from '../../components/Container/Container';
 import Title from '../../components/Title/Title';
@@ -7,6 +10,12 @@ import Input from '../../components/Input/Input';
 import styles from './Contact.module.scss';
 
 const Contact = () => {
+  const [shop, setShop] = useState<IShop | null>(null);
+
+  useEffect(() => {
+    getMainShop().then((data) => setShop(data));
+  }, []);
+
   const [form, setForm] = useState({
     first_name: '',
     last_name: '',
@@ -39,7 +48,7 @@ const Contact = () => {
 
             <h2 className={styles.title}>Ünvan</h2>
 
-            <p className={styles.text}>H.Əliyev prospekti 21902a Sumqayit şəhəri</p>
+            <p className={styles.text}>{shop?.address}</p>
           </div>
 
           <div className={styles.box}>
@@ -50,8 +59,8 @@ const Contact = () => {
             <h2 className={styles.title}>Əlaqə</h2>
 
             <p className={styles.text}>
-              Telefon: 018 65 11 22 33 <br />
-              Email: info@sufle.com
+              Telefon: {shop?.phone} <br />
+              Email: {shop?.email}
             </p>
           </div>
 
@@ -62,16 +71,12 @@ const Contact = () => {
 
             <h2 className={styles.title}>İş Saatları</h2>
 
-            <p className={styles.text}>B.e - Bazar: 09:00-21:00</p>
+            <p className={styles.text}>{shop?.working_hours}</p>
           </div>
         </div>
 
         <div className={styles.map}>
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d79043.10255345958!2d19.588687491104825!3d51.74955063559649!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471bcb56c4119b59%3A0xfca7f17fec34f65b!2sManufaktura!5e0!3m2!1sen!2spl!4v1686474654372!5m2!1sen!2spl"
-            width="100%"
-            height="100%"
-          ></iframe>
+          <iframe src={shop?.map_url} width="100%" height="100%"></iframe>
         </div>
 
         <form className={styles.form} onSubmit={handleSubmit}>
