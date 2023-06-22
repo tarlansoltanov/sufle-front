32086497778e80bb288b966e9f2d0863baa73f8f
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-06-22 21:02:37 +0200

    Shops page API functionality added

--- src/views/shops/index.tsx
+++ src/views/shops/index.tsx
@@ -1,4 +1,8 @@
+import { useState, useEffect } from 'react';
 import cs from 'classnames';
+
+import { IShop } from '../../types';
+import { getAllShops } from '../../api';
 
 import Title from '../../components/Title/Title';
 import Container from '../../components/Container/Container';
@@ -6,85 +10,57 @@ import Container from '../../components/Container/Container';
 import styles from './Shops.module.scss';
 
 const Shops = () => {
+  const [shops, setShops] = useState<IShop[]>([]);
+
+  useEffect(() => {
+    getAllShops().then((data) => setShops(data || []));
+  }, []);
+
   return (
     <main>
       <Title title="Mağazalarımız" subtitle="Mağazalarımız" />
-      
+
       <Container>
         <div className={styles.wrapper}>
-          <div className={styles.box}>
-            <div className={styles.title}>
-              <h1>Sumqayıt filialı 1</h1>
-            </div>
+          {shops &&
+            shops.map((shop: IShop) => (
+              <div className={styles.box} key={shop.id}>
+                <div className={styles.title}>
+                  <h1>{shop.name}</h1>
+                </div>
 
-            <div className={styles.content}>
-              <div className={styles.widget}>
-                <h2>Ünvan</h2>
-                <p>Sumqaıt şəhəri, 18-ci mkr dairəsi 2465A-2287</p>
+                <div className={styles.content}>
+                  <div className={styles.widget}>
+                    <h2>Ünvan</h2>
+                    <p>{shop.address}</p>
+                  </div>
+
+                  <div className={styles.widget}>
+                    <h2>İş Saatları</h2>
+                    <p>{shop.working_hours}</p>
+                  </div>
+
+                  <div className={styles.widget}>
+                    <h2>Əlaqə</h2>
+                    <p>Telefon: {shop.phone}</p>
+                    {shop.email && <p>Email: {shop.email}</p>}
+                  </div>
+
+                  <div className={cs(styles.widget, styles.mapsUrl)}>
+                    <a href={shop.map_url} target="_blank">
+                      Google map-a keçid
+                    </a>
+                  </div>
+                </div>
+
+                <div className={styles.media}>
+                  <div className={styles.photo}>
+                    <div className={styles.overlay}></div>
+                    <img src={shop.photo} alt={shop.name} />
+                  </div>
+                </div>
               </div>
-
-              <div className={styles.widget}>
-                <h2>İş Saatları</h2>
-                <p>Bazar ertəsi - Bazar: 09:00-21:00</p>
-              </div>
-
-              <div className={styles.widget}>
-                <h2>Əlaqə</h2>
-                <p>Telefon: 018 65 11 22 33</p>
-                <p>Email: info@sufle.com</p>
-              </div>
-
-              <div className={cs(styles.widget, styles.mapsUrl)}>
-                <a href="https://maps.google.com" target="_blank">
-                  Google map-a keçid
-                </a>
-              </div>
-            </div>
-
-            <div className={styles.media}>
-              <div className={styles.photo}>
-                <div className={styles.overlay}></div>
-                <img src="/src/assets/images/shop/shop1.png" alt="Sumqayıt filialı" />
-              </div>
-            </div>
-          </div>
-
-          <div className={styles.box}>
-            <div className={styles.title}>
-              <h1>Sumqayıt filialı 1</h1>
-            </div>
-
-            <div className={styles.content}>
-              <div className={styles.widget}>
-                <h2>Ünvan</h2>
-                <p>Sumqaıt şəhəri, 18-ci mkr dairəsi 2465A-2287</p>
-              </div>
-
-              <div className={styles.widget}>
-                <h2>İş Saatları</h2>
-                <p>Bazar ertəsi - Bazar: 09:00-21:00</p>
-              </div>
-
-              <div className={styles.widget}>
-                <h2>Əlaqə</h2>
-                <p>Telefon: 018 65 11 22 33</p>
-                <p>Email: info@sufle.com</p>
-              </div>
-
-              <div className={cs(styles.widget, styles.mapsUrl)}>
-                <a href="https://maps.google.com" target="_blank">
-                  Google map-a keçid
-                </a>
-              </div>
-            </div>
-
-            <div className={styles.media}>
-              <div className={styles.photo}>
-                <div className={styles.overlay}></div>
-                <img src="/src/assets/images/shop/shop1.png" alt="Sumqayıt filialı" />
-              </div>
-            </div>
-          </div>
+            ))}
         </div>
       </Container>
     </main>
