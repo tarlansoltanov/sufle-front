5c3a96f16e173de54d6628f8bdf2174b6c90c790
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-06-24 20:59:18 +0200

    Product Details Page Data added with Responsive

--- src/views/product/index.tsx
+++ src/views/product/index.tsx
@@ -1,3 +1,4 @@
+import { useParams } from 'react-router-dom';
 import ImageGallery from 'react-image-gallery';
 import { useRef } from 'react';
 
@@ -8,6 +9,8 @@ import styles from './Product.module.scss';
 import './image-gallery.css';
 
 const Product = () => {
+  const { id } = useParams();
+
   const imageGalleryRef = useRef<any>(null);
 
   const onClickHandler = () => {
@@ -45,6 +48,22 @@ const Product = () => {
               ref={imageGalleryRef}
               onClick={onClickHandler}
             />
+          </div>
+          <div className={styles.details}>
+            <div className={styles.price}>
+              <p className={styles.oldPrice}>20.00 AZN</p>
+              <p className={styles.currentPrice}>{20.0 * ((100 - 10) / 100)} AZN</p>
+              <p className={styles.discount}>10%</p>
+            </div>
+            <div className={styles.title}>
+              <h1>Karamelli Tort</h1>
+            </div>
+            <div className={styles.description}>
+              <p>
+                Un, yumurta, şəkər tozu, qara şokolad, bitki əsaslı qaymaq, fəsilə uyğun meyvələr,
+                manqo ətri, albalı jelesi, qabartma tozu, vanil tozu, jelatin, jele, emulqator, su.
+              </p>
+            </div>
           </div>
         </div>
       </Container>
