a48bc8e38990906cad792ed60dc3f35c715461eb
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-06-24 21:59:39 +0200

    Image Type added

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
 export type { default as IVacancy } from './vacancy';
 export type { default as IShop } from './shop';
 export type { default as ICategory } from './category';
+export type { default as IImage } from './image';
