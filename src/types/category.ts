ab3eda31ed3bbbe477c7a26bba796cb47721c494
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-06-28 21:28:24 +0200

    Category Logo Type updated

--- src/types/category.ts
+++ src/types/category.ts
@@ -1,7 +1,11 @@
 export interface ICategory {
   id: number;
   name: string;
-  logo?: string;
+  logo?: {
+    white: string;
+    grey: string;
+    red: string;
+  };
   main_category?: ICategory;
   sub_categories?: ICategory[];
   created_at: string;
@@ -10,7 +14,11 @@ export interface ICategory {
 export interface IMainCategory {
   id: number;
   name: string;
-  logo: string;
+  logo: {
+    white: string;
+    grey: string;
+    red: string;
+  };
   sub_categories: ISubCategory[];
   created_at: string;
 }
