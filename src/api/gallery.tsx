613372285e7742b7975c34fe13e5a3b74593fe6e
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-07-03 18:33:58 +0200

    Gallery API type filter added

--- src/api/gallery.tsx
+++ src/api/gallery.tsx
@@ -2,9 +2,9 @@ import instance from './index';
 
 import { IGallery } from '../types';
 
-const getGalleryItems = async (): Promise<IGallery[]> => {
+const getGalleryItems = async (type: 'image' | 'video' | null): Promise<IGallery[]> => {
   try {
-    const resp = await instance.get(`gallery`);
+    const resp = await instance.get(`gallery?${type ? `type=${type}` : ''}`);
     return resp.data as IGallery[];
   } catch (error) {
     throw error;
