024e0ec96b6414fb2aa1aed518c0284a89805377
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-07-03 16:09:37 +0200

    get Video ID and get Video Thumbnail added

---
+++ src/utils/gallery.ts
@@ -0,0 +1,13 @@
+const getVideoId = (url: string) => {
+  const urlParams = new URLSearchParams(new URL(url).search);
+  return urlParams.get('v');
+};
+
+export { getVideoId };
+
+const getVideoThumbnail = (url: string) => {
+  const videoId = getVideoId(url);
+  return videoId ? `https://img.youtube.com/vi/${videoId}/default.jpg` : null;
+};
+
+export { getVideoThumbnail };
