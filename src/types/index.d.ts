7d96c56cd1b300d2228e1f12b4f150fc3d4e1077
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-07-06 23:02:23 +0400

    Types and API files updated

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -3,5 +3,6 @@ export type { default as IShop } from './shop';
 export type { ICategory, IMainCategory, ISubCategory } from './category';
 export type { default as IImage } from './image';
 export type { default as IGallery } from './gallery';
+export type { default as IAdvert } from './advert';
 export type { IProduct, IPaginatedProducts } from './product';
 export type { IFilterProps, IPromoProps, IPaginationProps } from './extra';
