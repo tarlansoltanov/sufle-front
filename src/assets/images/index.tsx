080facc6412ef7dbd25eba1d538b85a81e063286
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-07-24 23:11:03 +0400

    Assets Updated

---
+++ src/assets/images/index.tsx
@@ -0,0 +1,7 @@
+import Logo from "./logo.png";
+
+import AboutImage1 from "./about/about1.png";
+import AboutImage2 from "./about/about2.png";
+import AboutImage3 from "./about/about3.png";
+
+export { Logo, AboutImage1, AboutImage2, AboutImage3 };
